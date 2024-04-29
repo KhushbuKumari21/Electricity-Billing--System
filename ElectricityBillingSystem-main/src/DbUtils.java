669b7759b735749cbe3a1947a459dcae56989ee5
Khushbu Kumari <56738986+KhushbuKumari21@users.noreply.github.com>
2024-04-29 22:16:12 +0530

    Add files via upload
    Project Title: Electricity Billing System
    Description: Developed Java-based software for Department of Electricity billing, enhancing efficiency.
    Contributions:
    Led design and development using Java.
    Utilized Swing and AWT for user-friendly interface.
    Integrated MySQL for data management.
    Implemented algorithms for consumption calculation.
    Conducted thorough testing for reliability.
    Achievements:
    Improved billing efficiency and accuracy.
    Enhanced consumer accessibility and comfort.
    Received positive stakeholder feedback.
    Technologies:
    Languages: Java
    Frameworks: Swing, AWT
    Databases: MySQL

---
+++ ElectricityBillingSystem-main/src/DbUtils.java
@@ -0,0 +1,14 @@
+package src;
+
+import java.sql.ResultSet;
+
+import javax.swing.table.TableModel;
+
+public class DbUtils {
+
+    public static TableModel resultSetToTableModel(ResultSet rs) {
+
+        throw new UnsupportedOperationException("Unimplemented method 'resultSetToTableModel'");
+    }
+
+}
